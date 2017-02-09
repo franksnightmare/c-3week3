6cfd9a4473c9c89e798761fcba3321dd6567bda7
Taragohf <ajos.chungos@gmail.com>
2017-02-09 15:33:46 +0100

    temporal 20

---
+++ a20/main.h
@@ -0,0 +1,97 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+//#include <algorithm> 
+//#include <typeinfo>
+
+enum Operators
+    {
+	LSH
+    };
+
+template <class Derived>
+struct Shl
+{
+    Derived &operator<<=(Derived const &rhs)
+    {
+	static_cast<Derived &>(*this).shlWrap(rhs);
+	return std::move(static_cast<Derived &>(*this));
+    }
+};
+
+//specialization
+
+template <class Derived, Operators ...ops>
+    class SBhandler
+{
+};
+
+template <class Derived, Operators ...ops>
+    class SBhandler<Derived, LSH, ops...>: public Shl<Derived>,SBhandler<Derived, ops...>
+{
+};
+
+template <class Derived>
+    class SBhandler<Derived>
+{
+};
+
+
+template <class Derived, Operators ...ops>
+    class ShiftBase: public SBhandler<Derived, ops...>
+{
+    friend Shl<Derived>;
+
+    //friend Derived operator<<Derived>(Derived &&lhs, Derived const &rhs);
+
+    void shlWrap(Derived const &rhs);//TODO
+    std::ostream &insertWrap(std::ostream &out) const;//TODO
+      
+};
+
+template <class Derived, Operators ...ops>
+    void ShiftBase<Derived, ops...>::shlWrap(Derived const &rhs)
+{
+    lshift(rhs);
+}
+
+class ShlInserter: public ShiftBase<ShlInserter, LSH>
+{
+    friend class ShiftBase<ShlInserter, LSH>;
+    int d_data;
+
+ public:
+    ShlInserter(int data);
+    void swap(ShlInserter &other);//TODO
+    int Data();
+    
+ private:
+    void lshift(ShlInserter const &lhs);
+};
+
+ShlInserter::ShlInserter(int data)
+:
+d_data(data)
+{}
+
+int ShlInserter::Data()
+{
+    return d_data;
+}
+void ShlInserter::lshift(ShlInserter const &lhs)
+{
+    d_data <<= lhs.d_data;
+    std::cout << "lshift called\n";
+}
+
+#endif
+
+int main()
+{
+    ShlInserter obj1 = 2;
+    ShlInserter	obj2 = 1;
+    obj1 <<= obj2;
+    std::cout << obj1.Data() << "\n";
+    
+}
