e90805cdde39aa156cc718121a384841fbb48c5e
Christiaan Steenkist <chnstone13backup@gmail.com>
2017-02-09 14:53:01 +0100

    prrrrogress

--- a21/ptr.h
+++ a21/ptr.h
@@ -1,43 +1,37 @@
 #ifndef PTR_H
 #define PTR_H
 
-#include <iostream>
-#include <typeinfo>
-
-template <typename T>
-struct Deleter
-{
-	T *d_ptr;
-	
-	Deleter(T *ptr)
-	:
-	d_ptr(ptr)
-	{}
-};
+#include <functional>
 
 template <typename Base>
 class Ptr
 {
 	Base *d_ptr;
+	std::function<void(Base*)> d_deleter;
 	
 	public:
+		// Set pointer and deleter
 		template <typename Derived>
 		Ptr(Derived *ptr)
 		:
-			d_ptr(ptr)
-		{
-			Deleter<Derived> del(ptr);
-		}
+			d_ptr(ptr),
+			d_deleter([](Base *ptr) {delete static_cast<Derived*>(ptr);})
+		{}
+		
+		// Call deleter on destruction
 		~Ptr()
 		{
-			delete d_ptr;
+			d_deleter(d_ptr);
 		}
 		
-		void reset(Base *basePtr)
+		// Call deleter and set the new pointer and deleter
+		template <typename Derived>
+		void reset(Derived *basePtr)
 		{
-			//delete static_cast<Base *>(d_ptr);
-			delete d_ptr;
+			d_deleter(d_ptr);
+			
 			d_ptr = basePtr;
+			d_deleter = [](Base *ptr) {delete static_cast<Derived*>(ptr);};
 		}
 };
 
---
+++ report/src/a21/ptr.h
@@ -0,0 +1,41 @@
+#ifndef PTR_H
+#define PTR_H
+
+#include <functional>
+
+template <typename Base>
+class Ptr
+{
+	Base *d_ptr;
+	std::function<void(Base*)> d_deleter;
+	
+	public:
+		// Set pointer and deleter
+		template <typename Derived>
+		Ptr(Derived *ptr)
+		:
+			d_ptr(ptr),
+			d_deleter([](Base *ptr) {
+				delete static_cast<Derived*>(ptr);})
+		{}
+		
+		// Call deleter on destruction
+		~Ptr()
+		{
+			d_deleter(d_ptr);
+		}
+		
+		// Call deleter and set the new pointer and
+		//  the new deleter
+		template <typename Derived>
+		void reset(Derived *basePtr)
+		{
+			d_deleter(d_ptr);
+			
+			d_ptr = basePtr;
+			d_deleter = [](Base *ptr) {
+				delete static_cast<Derived*>(ptr);};
+		}
+};
+
+#endif
